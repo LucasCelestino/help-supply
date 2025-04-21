d5566702a925692087ba5bfb31e4575323403280
Lucas Celestino <lucas_cap20@hotmail.com>
2025-04-21 11:39:34 -0300

    adicionando rotas na index

--- index.php
+++ index.php
@@ -14,15 +14,18 @@ $router = new Router(APP_URL);
 
 $router->namespace("App\Controllers\Web");
 
+//HOME
 $router->get("/home", "HomeController:index");
 $router->get("/", "HomeController:index");
 
 
+//LOGIN
 $router->get("/login", "LoginController:index");
 $router->post("/login", "LoginController:login");
 $router->get("/loggout", "LoginController:loggout");
 
 
+// CONTROLE DE NAVIOS
 $router->get("/navios-fornecidos", "ShipController:index");
 
 $router->get("/navios-fornecidos/exibir/{id}", "ShipController:show");
@@ -32,22 +35,29 @@ $router->get("/navios-fornecidos/excluir/{id}", "ShipController:destroy");
 $router->get("/navios-fornecidos/editar/{id}", "ShipController:edit");
 $router->post("/navios-fornecidos/editar", "ShipController:update");
 
-$router->post("/navios-fornecidos/adicionar", "ShipController:store");
-
 $router->get("/navios-fornecidos/adicionar", "ShipController:create");
 $router->post("/navios-fornecidos/adicionar", "ShipController:store");
 
 $router->get("/navios-fornecidos/pesquisar", "ShipController:search");
 
+// CONTROLE DE PASTAS
+$router->get("/controle-pastas", "FolderController:index");
 
-// $router->get("/users/{id}", "UserController:show");
+$router->get("/controle-pastas/exibir/{id}", "FolderController:show");
 
-$router->get("/users/create", "UserController:create");
-$router->post("/users/store", "UserController:store");
+$router->get("/controle-pastas/excluir/{id}", "FolderController:destroy");
 
-$router->get("/users/edit/{id}", "UserController:edit");
-$router->put("/users/update", "UserController:update");
+$router->get("/controle-pastas/editar/{id}", "FolderController:edit");
+$router->post("/controle-pastas/editar", "FolderController:update");
 
-$router->delete("/users/destroy/{id}", "UserController:destroy");
+$router->get("/controle-pastas/adicionar", "FolderController:create");
+$router->post("/controle-pastas/adicionar", "FolderController:store");
+
+$router->get("/controle-pastas/pesquisar", "FolderController:search");
+
+// LEMBRETES
+
+
+// PERFIL
 
 $router->dispatch();
